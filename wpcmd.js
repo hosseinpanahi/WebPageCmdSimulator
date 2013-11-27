5599d6e6037c7e508caac2aff413be1fa0d6299d
Milad Hosseinpanahi <hosseinpanahi@live.com>
2013-11-28 00:25:25 +0330

    Update wpcmd.js

--- wpcmd.js
+++ wpcmd.js
@@ -10,7 +10,7 @@ wpcmd.typedCommand = "";
 wpcmd.config.timeOut = 1200;
 wpcmd.config.breakTime = 500;
 wpcmd.config.maxCommandLength = 15;
-wpcmd.config.dontCaptureOnInputTags = true;
+wpcmd.config.dontCaptureOnInputElements = true;
 
 wpcmd.setPureCommand = function(cmdKeys, callback){
 	wpcmd.commands[cmdKeys] = callback;
@@ -25,38 +25,42 @@ wpcmd.clearKeys = function() {
 	wpcmd.typedCommand = "";
 };
 
+wpcmd.runCommand = function(name){
+	if(name.length > 0)
+	{
+		if(wpcmd.commands[name] !== null && wpcmd.commands[name] !== undefined) {
+			wpcmd.commands[name]();
+			wpcmd.clearKeys();
+		}
+	}
+};
+
 wpcmd.generalKeyUp = function (event) {
 	var _target = event.target || window.event.srcElement;
-	if (_target.tagName.toLocaleLowerCase() !== "input" || !wpcmd.config.dontCaptureOnInputTags) {
-		if (wpcmd.toBeClearedCommand !== null) {
-		    clearTimeout(wpcmd.toBeClearedCommand);
-		}
-		wpcmd.toBeClearedCommand = setTimeout(function () {
-		    wpcmd.clearKeys();
-		}, wpcmd.config.timeOut);
+    if (_target.tagName.toLocaleLowerCase() !== "input" || !wpcmd.config.dontCaptureOnInputElements) {
+        if (wpcmd.toBeClearedCommand !== null) {
+            clearTimeout(wpcmd.toBeClearedCommand);
+        }
+        wpcmd.toBeClearedCommand = setTimeout(function () {
+            wpcmd.clearKeys();
+        }, wpcmd.config.timeOut);
 		var _keyCode = event.keyCode || window.event.which;
+		
+        if (wpcmd.pressedKeys.length < wpcmd.config.maxCommandLength) {
+            wpcmd.pressedKeys += _keyCode;
+			wpcmd.typedCommand += String.fromCharCode(_keyCode).toLowerCase();
+        }
+        else
+            wpcmd.pressedKeys = _keyCode;
 			
-		if (wpcmd.pressedKeys.length < wpcmd.config.maxCommandLength) {			
-		    wpcmd.pressedKeys += _keyCode;
-				wpcmd.typedCommand += String.fromCharCode(_keyCode).toLowerCase();
-		}
-		else
-		    wpcmd.pressedKeys = _keyCode;
-		    
 		if(wpcmd.toBeWritingCommand !== null) {
 			clearTimeout(wpcmd.toBeWritingCommand);
 		}
 		wpcmd.toBeWritingCommand = setTimeout(function () {
-			if (wpcmd.commands[wpcmd.pressedKeys] !== undefined) {
-				wpcmd.commands[wpcmd.pressedKeys]();
-				wpcmd.clearKeys();
-			}
-			if (wpcmd.commands[wpcmd.typedCommand] !== undefined) {
-				wpcmd.commands[wpcmd.typedCommand]();
-				wpcmd.clearKeys();
-			}
-		}, wpcmd.config.breakTime);
-	}    
+			wpcmd.runCommand(wpcmd.pressedKeys);
+			wpcmd.runCommand(wpcmd.typedCommand);
+        }, wpcmd.config.breakTime);
+    }    
 };
 
 if(document.addEventListener) {
